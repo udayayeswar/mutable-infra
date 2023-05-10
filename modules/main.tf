4d333281f080ddd0bc6668d6eb538c14666c0463
udaye <udayeswari.savana@gmail.com>
2023-05-09 22:25:04 -0400

    terraform

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,39 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "var.vpc_cidr"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
