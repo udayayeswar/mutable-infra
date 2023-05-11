5efd88682b68fe8c137ae875250570585fa266e7
udaye <udayeswari.savana@gmail.com>
2023-05-11 11:34:27 -0400

    terraform

--- modules/main.tf
+++ modules/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "0.0.0.0/8"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "demo1"
