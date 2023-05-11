2fe9f8c40e0179fb5ee6db4918dcb0d428a26a8d
udaye <udayeswari.savana@gmail.com>
2023-05-11 12:34:24 -0400

    terraform

--- modules/main.tf
+++ modules/main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "web" {
   ami                = "ami-07acf41a58c76cc08"
   instance_type      = "t3.micro"
   subnet_id          = aws_subnet.subnet1.id
-   availability_zone = "us-east-1a"
+   availability_zone = "us-east-1c"
   tags = {
     Name = "demo-instance1"
   }
@@ -47,7 +47,7 @@ resource "aws_instance" "web1" {
   ami           = "ami-07acf41a58c76cc08"
   instance_type = "t3.micro"
   subnet_id     =aws_subnet.subnet2.id
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-1c"
   tags = {
     Name = "demo-instance2"
   }
@@ -56,7 +56,7 @@ resource "aws_route_table" "example" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "10.0.0.0/16"
     gateway_id = "igw-02d7e6a9c6afda95e"
   }
 
