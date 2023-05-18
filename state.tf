57d822b2650eb01eedc53cd018b0ec8fc37e8804
udaye <udayeswari.savana@gmail.com>
2023-05-18 12:26:51 -0400

    terraform

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "udaya-terraform-state"
-    key    = "mutable-infra/terraform.tfstate"
+    bucket = "terraform-backendstorage"
+    key    = "mutable/frontend/dev/terraform.tfstate"
     region = "us-east-1"
 
   }
