e59c286b3e9217007996024df32747b8e4dd6ed5
udaye <udayeswari.savana@gmail.com>
2023-05-18 15:38:40 -0400

    terraform

--- modules/main.tf
+++ modules/main.tf
@@ -7,17 +7,17 @@ resource "aws_vpc" "main" {
 
 }
 
-#resource "aws_subnet" "subnet1" {
-#  count      = 2
-#  vpc_id     = aws_vpc.main.id
-#  cidr_block = "10.0.1.0/24"
-#  availability_zone = "us-east-1a"
-#
-#  tags = {
-#    Name = "demo-${count.index}"
-#  }
-#}
-#
+resource "aws_subnet" "subnet1" {
+  count      = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = ["10.0.${count.index}.0/24"]
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "demo-${count.index}"
+  }
+}
+
 #resource "aws_subnet" "subnet2" {
 #  vpc_id     = aws_vpc.main.id
 #  cidr_block = "10.0.3.0/24"
