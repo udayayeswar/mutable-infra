c84ed3352911beea0f076450def296a238ab9eab
udaye <udayeswari.savana@gmail.com>
2023-05-20 11:18:30 -0400

    terraform

--- modules/main.tf
+++ modules/main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "subnet1" {
 
 resource "aws_subnet" "subnet2" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/16"
+  cidr_block = "10.0.0.0/16"
   availability_zone = "us-east-1a"
 
   tags = {
