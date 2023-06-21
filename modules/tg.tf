cc08345454104a0d06bd8a9e2931deb7e3fabeb6
udaye <udayeswari.savana@gmail.com>
2023-06-21 14:47:57 -0400

    terraform

--- modules/tg.tf
+++ modules/tg.tf
@@ -4,4 +4,27 @@ resource "aws_lb_target_group" "example" {
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "instance"
+
+
+  health_check {
+    enabled   = true
+    port      = 1337
+    interval  = 6
+    timeout   = 5
+    healthy   = 2
+    unhealthy = 2
+  }
+}
+
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
 }
